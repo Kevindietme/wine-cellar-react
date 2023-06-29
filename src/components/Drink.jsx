88052655afb47b6c7b529afe3d79098ec67bd1e5
Kevin Dietmeyer <kevindietmeyer@Kevins-MacBook-Air-2.local>
2023-06-29 16:23:41 -0400

    added base css

--- src/components/Drink.jsx
+++ src/components/Drink.jsx
@@ -2,52 +2,54 @@ import { useState } from "react"
 
 
 export default function Drink() {
-    const [drinkType, setDrinkType] = useState("reds");
-    const [drinkList, setDrinkList] = useState();
-    
+  const [drinkType, setDrinkType] = useState("reds");
+  const [drinkList, setDrinkList] = useState();
 
 
-const getDrink = async(drinkType) => {
+
+  const getDrink = async (drinkType) => {
     const res = await fetch(`https://api.sampleapis.com/wines/${drinkType}`);
     const data = await res.json();
     setDrinkList(data);
-}
+  }
 
-    return(
-        <section>
-          <nav>
-            <button onClick={ () => setDrinkType('reds') }>Red</button>
-            <button onClick={ () => setDrinkType('whites') }>White</button>
-            <button onClick={ () => setDrinkType('dessert') }>Dessert</button>
-            <button onClick={ () => setDrinkType('sparkling') }>Sparkling</button>
-            <button onClick={ () => setDrinkType('port') }>Port</button>
-            <button onClick={ () => setDrinkType('rose') }>Rose</button>
-          </nav>
+  return (
+    <section>
+      <nav>
+        <button onClick={() => setDrinkType('reds')}>Red</button>
+        <button onClick={() => setDrinkType('whites')}>White</button>
+        <button onClick={() => setDrinkType('dessert')}>Dessert</button>
+        <button onClick={() => setDrinkType('sparkling')}>Sparkling</button>
+        <button onClick={() => setDrinkType('port')}>Port</button>
+        <button onClick={() => setDrinkType('rose')}>Rose</button>
+      </nav>
 
-          <div>
-            <button onClick={ ()=> getDrink(drinkType)}>Get Drink</button>
-          </div>
+      <div className="button-getdrink">
+        <button onClick={() => getDrink(drinkType)}>Get Drink</button>
+      </div>
 
+      <div className="drinkContainer">
         <h2>{drinkType}</h2>
-          <div className="drink-list">
+        <div className="drink-list">
 
-            {
-                !drinkList
-                    ? <p>No drinks available</p>
-                    : drinkList.map(  (element,index) => {
-                        return(
-                            <div key={index}>
-                                <img src={element.image} alt="cover" />
-                                <h3>element.wine</h3>
-                                <p>Location: {element.location}</p>
-                                <p>Rating: {element.rating.average}</p>
-                                <p>Reviews: {element.rating.reviews}</p>
-                            </div>    
-                        )
-                    })
-            }
+          {
+            !drinkList
+              ? <p>No drinks available</p>
+              : drinkList.map((element, index) => {
+                return (
+                  <div key={index} className="hover-effect">
+                    <img src={element.image} alt="cover" />
+                    <h3>element.wine</h3>
+                    <p>Location: {element.location}</p>
+                    <p>Rating: {element.rating.average}</p>
+                    <p>Reviews: {element.rating.reviews}</p>
+                  </div>
+                )
+              })
+          }
 
-          </div>
-        </section>
-    )
+        </div>
+      </div>
+    </section>
+  )
 }
